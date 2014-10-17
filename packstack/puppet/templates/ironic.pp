04c4da15efd6ddb5b90a35263aa3831c54719922
Ryan Hallisey <rhallisey@localhost.localdomain>
2014-10-17 11:44:51 -0400

    Change configuration in ironic.pp template

--- packstack/puppet/templates/ironic.pp
+++ packstack/puppet/templates/ironic.pp
@@ -7,13 +7,4 @@ class { 'ironic::api':
 class { 'ironic::conductor':
 }
 
-class { 'ironic::keystone::domain':
-      auth_url          => 'http://%(CONFIG_CONTROLLER_HOST)s:35357/v2.0',
-      keystone_admin    => 'admin',
-      keystone_password => '%(CONFIG_KEYSTONE_ADMIN_PW)s',
-      keystone_tenant   => 'admin',
-      domain_name       => '%(CONFIG_IRONIC_DOMAIN)s',
-      domain_admin      => '%(CONFIG_IRONIC_DOMAIN_ADMIN)s',
-      domain_password   => '%(CONFIG_IRONIC_DOMAIN_PASSWORD)s',
-}
 
