c665061f1f1166723216f2ceca0c966395e37b41
Ryan Hallisey <rhallise@redhat.com>
2014-10-20 09:48:20 -0400

    Additional changes to ironic.pp
    Remove unused variables

--- packstack/puppet/templates/ironic.pp
+++ packstack/puppet/templates/ironic.pp
@@ -1,58 +1,26 @@
-#$db_host = 'db'
-#$db_username = 'ironic'
-#$db_password = 'password'
-#$rabbit_user = 'ironic'
-#$rabbit_password = 'ironic'
-#$rabbit_port = '5672'
-#$glance_api_servers = 'glance:9292'
 
 $deploy_kernel = 'glance://deploy_kernel_uuid'
 $deploy_ramdisk = 'glance://deploy_ramdisk_uuid'
-$rabbit_vhost = '/'
-$rabbit_hosts = ['rabbitmq:5672']
+#$rabbit_vhost = '/'
+#$rabbit_hosts = ['rabbitmq:5672']
 #$db_name = 'ironic'
 
-node db {
-  class { 'mysql::server':
-    config_hash => {
-      'bind_address' => '0.0.0.0'
-    }
-  }
 
-  class { 'mysql::ruby': }
+class { 'ironic':
+  #    db_name => $db_name,
+  auth_host => "%(CONFIG_CONTROLLER_HOST)s",
+  admin_password => "%(CONFIG_IRONIC_KS_PW)s",
+#  rabbit_virtual_host => $rabbit_vhost,
+#  rabbit_hosts => $rabbit_hosts,
 
-  class { 'ironic::db::mysql':
-    password => $db_password,
-    dbname => $db_name,
-    user => $db_username,
-    host => $clientcert,
-    allowed_hosts => ['controller'],
-  }
 }
+class { 'ironic::api': }
 
-node controller {
-  class { 'ironic':
-#    db_name => $db_name,
-    rabbit_virtual_host => $rabbit_vhost,
-    rabbit_hosts => $rabbit_hosts,
+class { 'ironic::conductor': }
 
-#    db_password => $db_password,
-#    db_user => $db_username,
-#    db_host => $db_host,
-#    rabbit_password => $rabbit_password,
-#    rabbit_userid => $rabbit_user,
-#    glance_api_servers => $glance_api_servers,
-  }
-  class { 'ironic::api': }
+class { 'ironic::drivers::ipmi': }
 
-  class { 'ironic::conductor': }
-
-  class { 'ironic::drivers::ipmi': }
-
-  class { 'ironic::drivers::pxe':
-    deploy_kernel => $deploy_kernel,
-    deploy_ramdisk => $deploy_ramdisk,
-  }
+class { 'ironic::drivers::pxe':
+  deploy_kernel => $deploy_kernel,
+  deploy_ramdisk => $deploy_ramdisk,
 }
-
-
