622bc8cdac288407d218b7cf3fc1b7baf460959d
Ryan Hallisey <rhallise@redhat.com>
2014-10-21 09:37:04 -0400

    Changes to the Ironic manifest
    Add auth_url to ironic-api and ironic-conductor. Also,
    cleanup unused code.

--- packstack/puppet/templates/ironic.pp
+++ packstack/puppet/templates/ironic.pp
@@ -1,26 +1,10 @@
 
-$deploy_kernel = 'glance://deploy_kernel_uuid'
-$deploy_ramdisk = 'glance://deploy_ramdisk_uuid'
-#$rabbit_vhost = '/'
-#$rabbit_hosts = ['rabbitmq:5672']
-#$db_name = 'ironic'
-
-
 class { 'ironic::api':
-  #    db_name => $db_name,
   auth_host => "%(CONFIG_CONTROLLER_HOST)s",
   admin_password => "%(CONFIG_IRONIC_KS_PW)s",
-#  rabbit_virtual_host => $rabbit_vhost,
-#  rabbit_hosts => $rabbit_hosts,
-
+  auth_url => 'http://%(CONFIG_CONTROLLER_HOST)s:35357/v2.0',
 }
-class { 'ironic': }
 
-class { 'ironic::conductor': }
-
-class { 'ironic::drivers::ipmi': }
-
-class { 'ironic::drivers::pxe':
-  deploy_kernel => $deploy_kernel,
-  deploy_ramdisk => $deploy_ramdisk,
+class { 'ironic::conductor':
+  auth_url => 'http://%(CONFIG_CONTROLLER_HOST)s:35357/v2.0',
 }
